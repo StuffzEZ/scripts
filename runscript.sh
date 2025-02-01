6c670b663e0e36a16c35823e523ce6ee52963dd5
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2025-02-01 17:08:43 +0000

    Create runscript.sh

---
+++ runscript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check if argument is provided
+if [ -z "$1" ]; then
+    echo "No argument provided!"
+    exit 1
+fi
+
+arg1="$1"
+arg2="$2"
+# Check the value of the argument
+case "$1" in
+    "sl")
+        echo "Running sl with args"
+        curl -s "https://raw.githubusercontent.com/StuffzEZ/scripts/refs/heads/main/sl-runner.sh" "$arg2"
+    "packin")
+        echo "Running packin witb args"
+        curl -s "https://raw.githubusercontent.com/StuffzEZ/scripts/refs/heads/main/package-runner-installer/script.sh" "$arg2"
+        ;;
+    *)
+        echo "Unknown argument: $1"
+        exit 2
+        ;;
+esac
