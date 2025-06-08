0ec26f979a40763e8536e1d6ac723e93a539edd1
StuffzEZ <158745985+StuffzEZ@users.noreply.github.com>
2025-06-08 15:18:55 +0100

    Update serverstart.sh

--- serverstart.sh
+++ serverstart.sh
@@ -1,2 +1,16 @@
 curl -fsSL https://get.casaos.io | sudo bash
-echo Installed CasaOS.
+echo "Installed CasaOS!"
+echo "Allowing Ports..."
+sudo ufw allow 80
+echo "Allowed CasaOS Ports! - 80"
+sudo ufw allow 8080
+sudo ufw allow 8000
+sudo ufw allow 8111
+sudo ufw allow 8443
+sudo ufw allow 22/tcp
+sudo ufw allow 25565/tcp
+sudo ufw allow 25566:25570/tcp
+echo "Allowed Crafty Ports!"
+echo "Making STFZ File..."
+echo "StuffzEZ/scripts/serverstart.sh was here. STFZID: 369568734tdvrty7842b" > /home/ubuntu/stfzscript.serverstart.sh.stfz
+echo "Finished!"
